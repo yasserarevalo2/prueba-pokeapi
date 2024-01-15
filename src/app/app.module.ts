afab77675f51d916ff7b6602d1e160eecf58f985
Yasser arevalo <yarevalo@sistemasinfotec.com.co>
2024-01-15 17:07:41 -0500

    ini

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PokeDetailComponent } from './components/poke-detail/poke-detail.component';
+import { PokeTableComponent } from './components/poke-table/poke-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PokeDetailComponent,
+    PokeTableComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/poke-table/poke-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-table',
+  templateUrl: './poke-table.component.html',
+  styleUrls: ['./poke-table.component.scss']
+})
+export class PokeTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
